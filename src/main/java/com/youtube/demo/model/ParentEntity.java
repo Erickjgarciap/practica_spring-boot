6cd09b578c4c38a55aef532726133191dfed9380
macos <macos@Mac-mini-de-macos.local>
2018-08-11 17:44:59 -0500

    primeros pasos spring-boot

---
+++ src/main/java/com/youtube/demo/model/ParentEntity.java
@@ -0,0 +1,37 @@
+package com.youtube.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Access(AccessType.FIELD) /* Poner anotaciones de campo  */
+
+public class ParentEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3192962725288588853L;
+	//private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", unique=true, nullable=false)
+	private Long id;
+
+	public  Long getId() {
+		return id;
+	}
+
+	public  void setId(Long id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/main/java/com/youtube/demo/model/User.java
@@ -0,0 +1,85 @@
+package com.youtube.demo.model;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+@Access(AccessType.FIELD)
+
+public class User extends ParentEntity{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="first_name",nullable=false, length=255)
+	private String firstname;
+	
+	@Column(name="second_name",nullable=true, length=255)
+	private String secondname;
+	
+	@Column(name="first_surname",nullable=false, length=255)
+	private String firstsurname;
+	
+	@Column(name="second_surname",nullable=true, length=255)
+	private String secondsurname;
+	
+	@Column(name="phone",nullable=true, length=30)
+	private String phone;
+	
+	@Column(name="adrres",nullable=false, length=150)
+	private String adrres;
+
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getSecondname() {
+		return secondname;
+	}
+
+	public void setSecondname(String secondname) {
+		this.secondname = secondname;
+	}
+
+	public String getFirstsurname() {
+		return firstsurname;
+	}
+
+	public void setFirstsurname(String firstsurname) {
+		this.firstsurname = firstsurname;
+	}
+
+	public String getSecondsurname() {
+		return secondsurname;
+	}
+
+	public void setSecondsurname(String secondsurname) {
+		this.secondsurname = secondsurname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAdrres() {
+		return adrres;
+	}
+
+	public void setAdrres(String adrres) {
+		this.adrres = adrres;
+	}
+
+
+}
